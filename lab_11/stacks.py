96765106ec7059029faa4e80bf0037a4f4df7a2d
mumat0103 <wsx2138@naver.com>
2022-10-17 20:44:21 +0900

    Edit: add Node & CircularDoublyLinkedList in stacks.py & queues.py

--- lab_11/stacks.py
+++ lab_11/stacks.py
@@ -1,5 +1,151 @@
 #20180269 천성규
-from circular_doubly_linked_list import *
+class Node:
+    def __init__(self, item=None):
+        self.item = item
+        self.llink = self.rlink = None
+
+    def __eq__(self, other):
+        if not isinstance(other, Node):
+            return False
+
+        if self is other or self.item == other.item:
+            return True
+        return False
+
+    def __str__(self):
+        return f"{self.item}"
+
+    def __repr__(self):
+        return str(self)
+
+class CircularDoublyLinkedList:
+    def __init__(self):
+        self.head = None
+
+    def add_head(self, node_new):
+        if self.head is None:
+            self.head = node_new
+            node_new.rlink = node_new.llink = node_new
+        else:
+            node_new.llink = self.head.llink
+            node_new.rlink = self.head
+            self.head.llink.rlink = node_new
+            self.head.llink = node_new
+            
+            self.head = node_new
+
+    def add_tail(self, node_new):
+        if self.head is None:
+            self.head = node_new
+            node_new.rlink = node_new.llink = node_new
+        else:
+            node_new.llink = self.head.llink
+            node_new.rlink = self.head
+            self.head.llink.rlink = node_new
+            self.head.llink = node_new
+
+    def delete_head(self):
+        if self.is_empty():
+            raise Exception("list is empty.")
+        if self.head == self.head.llink == self.head.rlink:
+            self.head = None
+        else:
+            self.head.llink.rlink = self.head.rlink
+            self.head.rlink.llink = self.head.llink
+            self.head = self.head.rlink
+
+    def delete_tail(self):
+        if self.is_empty():
+            raise Exception("list is empty.")
+        
+        if self.head == self.head.llink == self.head.rlink:
+            self.head = None
+        else:
+            tail = self.head.llink
+            tail.llink.rlink = self.head
+            self.head.llink = tail.llink
+
+    def insert_after(self, node, node_new):
+        if self.is_empty():
+            raise Exception("list is empty.")
+        
+        temp = self.head
+
+        while str(temp) != str(node):
+            temp = temp.rlink
+
+        node_new.llink = temp
+        node_new.rlink = temp.rlink
+        temp.rlink.llink = node_new
+        temp.rlink = node_new
+
+
+    def insert_before(self, node, node_new):
+        if self.is_empty():
+            raise Exception("list is empty.")
+        
+        temp = self.head
+
+        while str(temp) != str(node):
+            temp = temp.rlink
+
+        if temp is self.head:
+            self.add_head(node_new)
+        else:
+            node_new.rlink = temp
+            node_new.llink = temp.llink
+            temp.llink.rlink = node_new
+            temp.llink = node_new
+
+    def delete(self, node):
+        if self.is_empty():
+            raise Exception("list is empty.")
+        
+        temp = self.head
+
+        while str(temp) != str(node):
+            temp = temp.rlink
+
+        if temp == self.head:
+            self.delete_head()
+            return
+        if temp == self.head.llink:
+            self.delete_tail()
+            return
+        prev = temp.llink
+        next = temp.rlink
+        prev.rlink = next
+        next.llink = prev
+
+    def __iter__(self):
+        self.current = self.head.llink
+        self.flag = 0
+        return self
+
+    def __next__(self):
+        self.current = self.current.rlink
+        if self.current is self.head and self.flag == 1:
+            raise StopIteration
+        if self.current is self.head and self.flag == 0:
+            self.flag = 1
+
+        return self.current
+
+    def is_empty(self):
+        return True if self.head == None else False
+    
+    def __str__(self):
+        result = []
+        if not self.is_empty():
+            temp = self.head
+            result.append(temp.item)
+            temp = temp.rlink
+            while temp != self.head:
+                result.append(temp.item)
+                temp = temp.rlink                
+            return f"{result}"
+        else:
+            return f"{result}"
 
 class Stack:
     def __init__(self):
